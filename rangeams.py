b34661d05590a0a000a40aac38f4749626882f2c
Aishu67 <52395134+Aishu67@users.noreply.github.com>
2019-07-13 19:21:00 +0530

    Update rangeams.py

--- rangeams.py
+++ rangeams.py
@@ -1,10 +1,10 @@
 x,y=map(int,input().split())
 res=[]
-s=0
 for num in range(x+1,y):
-   for i in range(len(str(num))):
-      s+=pow(num[i],3)
+   snum=str(num)
+   s=0
+   for i in range(len(snum)):
+      s+=pow(int(snum[i]),3)
    if s==num:
        res.append(num)
 print(*res)
-   
