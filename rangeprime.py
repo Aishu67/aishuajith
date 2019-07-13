0b68b5d72a51e7d9a05ae49ac1db8d13ba946221
Aishu67 <52395134+Aishu67@users.noreply.github.com>
2019-07-13 18:12:01 +0530

    Update rangeprime.py

--- rangeprime.py
+++ rangeprime.py
@@ -1,12 +1,12 @@
-x,y=map(int,input().split
+x,y=map(int,input().split())
 res=[]
 for i in range(x+1,y):
     if i>1:
-      for j in range(i):
+      for j in range(2,i):
          if i%j==0:
-            continune
-       else:
-         res.appen(i)
-     else:
+            break
+      else:
+         res.append(i)
+    else:
         continue
 print(*res)
