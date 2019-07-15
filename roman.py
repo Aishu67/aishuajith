babec1d1eae045dd15e76197af7579c77a82efda
Aishu67 <52395134+Aishu67@users.noreply.github.com>
2019-07-15 14:05:59 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,5 @@
+rl=input()
+r=("I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX")
+n=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+val=dict(zip(r,n))
+print(val[rl])
