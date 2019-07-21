1b022258d56531eeb41175d137a2109f40d09de9
Aishu67 <52395134+Aishu67@users.noreply.github.com>
2019-07-21 18:37:44 +0530

    Create Camelcase.java

---
+++ Camelcase.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Camelcase{
+    public static void main(String arg[]){
+        
+        String s;
+        int i;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        
+        char[] arr=s.toCharArray();
+        arr[0]=Character.toUpperCase(arr[0]);
+        for(i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                arr[i+1]=Character.toUpperCase(arr[i+1]);
+            }
+        }
+
+            System.out.println(arr);
+        
+    }
+}
