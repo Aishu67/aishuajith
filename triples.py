bf8ed12396e50b0a5ed8ca98aa9611a4ea2bb67a
Aishu67 <52395134+Aishu67@users.noreply.github.com>
2019-07-26 16:41:20 +0530

    Update triples.py

--- triples.py
+++ triples.py
@@ -1,9 +1,12 @@
 n=int(input())
 arr=list(map(int,input().split()))
 c=0
-for i in range(n-2):
+if len(arr)==n:
+ for i in range(n-2):
     for j in range(i+1,n-1):
         for k in range(i+j,n):
           if arr[i]>arr[j]>arr[k] and i<j<k:
               c+=1
-print(c)
+ print(c)
+else:
+    print(1)
