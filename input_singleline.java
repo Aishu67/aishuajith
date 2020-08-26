30fc5b02e1f5ddd0c56bac2f2081e68387fac04f
Aishu67 <52395134+Aishu67@users.noreply.github.com>
2020-08-26 22:52:24 +0530

    Create input_singleline.java

---
+++ input_singleline.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String[] s=sc.nextLine().split(" ");
+	    int a[]=new int[s.length];
+	    int i;
+	    for(i=0;i<s.length;i++){
+	        a[i]=Integer.parseInt(s[i]);
+	    }
+	    
+	}
+}
